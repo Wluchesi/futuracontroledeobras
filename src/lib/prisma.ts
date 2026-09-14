9a082338f1704b3dafe927b6e9fe5c8cfa3fbd6b
Wellington Luchesi <wluchesi@gmail.com>
2026-09-14 07:53:23 -0300

    fix(deploy): incluir .env no repositorio para leitura direta do Netlify

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import { loadEnvConfig } from '@next/env';
+
+loadEnvConfig(process.cwd());
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
